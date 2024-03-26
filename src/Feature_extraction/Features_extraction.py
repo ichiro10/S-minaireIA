24ed0555839e46002305a5b4f09dedfa1f05ae85
Ali Ghammaz <91700499+ichiro10@users.noreply.github.com>
2024-03-26 02:44:22 -0400

    4 features in one file

--- src/Feature_extraction/Features_extraction.py
+++ src/Feature_extraction/Features_extraction.py
@@ -272,6 +272,90 @@ def head_nods(video_path):
 
     return nods
 
+def overall_motion(video_path):
+
+    #video_path = "C://Users//aghammaz//Desktop//SIA//SeminaireIA//src//Feature_extraction//ex.mp4"
+
+    cap = cv2.VideoCapture(video_path) 
+    ret, first_frame = cap.read() 
+    
+
+    # Converts frame to grayscale
+    prev_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY) 
+
+    # Creates an image filled with zero 
+    mask = np.zeros_like(first_frame) 
+
+    # Sets image saturation to maximum 
+    mask[..., 1] = 255
+
+    # Get original frame rate
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    print('fps:',fps)
+    delay = int(1000 / fps)
+
+    # List to store magnitudes
+    magnitudes = []
+
+    while(cap.isOpened()): 
+        
+        ret, frame = cap.read() 
+        if not ret:
+           break
+        
+        # Opens a new window and displays the input frame 
+        cv2.imshow("input", frame) 
+        
+        # Converts each frame to grayscale 
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+        
+        # Calculates dense optical flow by Farneback method 
+        flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, 
+                                        None, 
+                                        0.5, 3, 15, 3, 5, 1.2, 0) 
+        
+        # Computes the magnitude and angle of the 2D vectors 
+        magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1]) 
+        
+        # Append magnitude to the list
+        magnitudes.append(magnitude)
+        
+        # Sets image hue according to the optical flow direction 
+        mask[..., 0] = angle * 180 / np.pi / 2
+        
+        # Sets image value according to the optical flow  magnitude (normalized) 
+        mask[..., 2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX) 
+        
+        # Converts HSV to RGB (BGR) color representation 
+        rgb = cv2.cvtColor(mask, cv2.COLOR_HSV2BGR) 
+        
+        # Opens a new window and displays the output frame 
+        cv2.imshow("dense optical flow", rgb) 
+        
+        # Updates previous frame 
+        prev_gray = gray 
+        
+        # Frames are read by intervals of 1 millisecond
+        if cv2.waitKey(1) & 0xFF == ord('q'): 
+            break
+        
+
+    cap.release() 
+    cv2.destroyAllWindows() 
+
+    # Convert magnitudes list to numpy array
+    magnitudes = np.array(magnitudes)
+
+    # Calculate average magnitude across all frames
+    average_magnitude = np.mean(magnitudes)
+
+    # Calculate temporal dynamics: Standard deviation of magnitudes over time
+    temporal_dynamics = np.std(magnitudes)
+
+
+    return average_magnitude, temporal_dynamics
+
+
 
 video_path = "C://Users//ghamm//OneDrive//Bureau//UQAC//Hiver//Séminaire//Feature_extraction//test1.mp4"
 number_frames(video_path)
@@ -281,3 +365,8 @@ print(h_rot)
 
 h_nods = head_nods(video_path)
 print(h_nods)
+
+average_magnitude, temporal_dynamics = overall_motion(video_path)
+
+print ("average_magnitude:",average_magnitude)
+print ("temporal_dynamics:",temporal_dynamics)
